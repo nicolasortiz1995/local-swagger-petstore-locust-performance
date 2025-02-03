c89caf3960eb988136555be8859d73711b88259a
nicolasortiz1995 <nortiz@coordinadora.com>
2025-02-03 10:48:56 -0500

    Final documentation

--- performance/create_user_post.py
+++ performance/create_user_post.py
@@ -3,6 +3,8 @@ import time
 import logging
 from datetime import datetime
 from pathlib import Path
+from faker import Faker
+import random
 from locust import HttpUser, task, between
 
 # Logging configuration
@@ -16,10 +18,12 @@ LOGS_DIR = PROJECT_ROOT / "Logs"
 # Create directories if they don't exist
 DATA_DIR.mkdir(parents=True, exist_ok=True)
 LOGS_DIR.mkdir(parents=True, exist_ok=True)
-LOG_FILE = LOGS_DIR / f'Find_pet_by_ID_GET_{datetime.now().strftime("%Y%m%d_%H%M%S")}.csv'
+LOG_FILE = LOGS_DIR / f'Create_user_post_{datetime.now().strftime("%Y%m%d_%H%M%S")}.csv'
 
 # Request headers
 HEADERS = {'Content-Type': 'application/json'}
+
+fake = Faker()
 
 def log_to_csv(data):
     """Write data to the log CSV file."""
@@ -33,14 +37,25 @@ log_to_csv(["REQUEST_DATE", "REQUEST_TIME", "STATUS_CODE", "RESPONSE_SIZE_MB",
 
 class WebsiteUser(HttpUser):
     host = "http://localhost:8080/"
-    wait_time = between(1, 2)
+    wait_time = between(0.5, 1)
 
     @task
     def api_request(self):
         try:
-            API_URL = "http://localhost:8080/api/v3/pet/1"
+            username = fake.user_name()
+            API_URL = "http://localhost:8080/api/v3/user"
+            payload = {
+                "id": random.randint(10000, 99999), 
+                "username": f"{username}",
+                "firstName": f"{fake.first_name()}",
+                "lastName": f"{fake.last_name()}",
+                "email": f"{fake.email()}",
+                "password": "testpass",
+                "phone": f"{fake.phone_number()}",
+                "userStatus": random.choice([0, 1])
+            }
             start_time = time.time()
-            response = self.client.get(url=API_URL, headers=HEADERS, name="TC1S4")  
+            response = self.client.post(url=API_URL, json=payload, headers=HEADERS, name="TC1S1")  
             end_time = time.time()
 
             log_data = [
@@ -49,7 +64,7 @@ class WebsiteUser(HttpUser):
                 response.status_code,
                 str(round(len(response.content) / (1024 * 1024), 2)).replace('.', ','), 
                 str(round(end_time - start_time, 1)).replace('.', ','),
-                "No parameters applied."
+                payload
             ]
             log_to_csv(log_data)
 
@@ -58,7 +73,5 @@ class WebsiteUser(HttpUser):
             log_to_csv([datetime.now().strftime('%Y-%m-%d'), datetime.now().strftime('%H:%M:%S'), "-", "-", "-", "-", "-", str(e)])
 
 # EXECUTION PARAMETERS:
-# locust -f find_pet_by_id_get.py --users 1 --web-port=8090 --spawn-rate 1 --run-time 30m
-# locust -f find_pet_by_id_get.py --users 50 --web-port=8090 --spawn-rate 0.16 --run-time 15m
-# locust -f find_pet_by_id_get.py --users 150 --web-port=8090 --spawn-rate 0.5 --run-time 15m
-# locust -f find_pet_by_id_get.py --users 500 --web-port=8090 --spawn-rate 0.55 --run-time 30m
+# locust -f create_user_post.py --users 150 --web-port=8090 --spawn-rate 0 --run-time 60m
+# locust -f create_user_post.py --users 50 --web-port=8090 --spawn-rate 0 --run-time 30m
